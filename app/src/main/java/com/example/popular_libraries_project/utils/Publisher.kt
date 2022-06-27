4bb466668ee9526fbfed18092cd434f6a83db5e8
Котик Дмитрий Степанович <DKotik@cbgr.ru>
2022-06-27 10:22:19 +0300

    lesson3 Создан класс реализации механизма подписки

---
+++ app/src/main/java/com/example/popular_libraries_project/utils/Publisher.kt
@@ -0,0 +1,32 @@
+package com.example.popular_libraries_project.utils
+
+import android.os.Handler
+
+private data class Subscriber<T>(
+    private val handler: Handler,
+    private val callback: (T?) -> Unit
+) {
+    fun invoke(value: T?) {
+        handler.post {
+            callback.invoke(value)
+        }
+    }
+}
+
+
+class Publisher<T>(
+    private val isSingle: Boolean = false
+) {
+    private val subscribers: MutableSet<Subscriber<T?>> = mutableSetOf()
+    var value: T? = null
+        private set
+    private var hasFirstValue = false
+
+    fun subscribe(handler: Handler, callback: (T?) -> Unit) {
+        val subscriber = Subscriber(handler, callback)
+        subscribers.add(subscriber)
+        if (hasFirstValue) {
+            subscriber.invoke(value)
+        }
+    }
+}

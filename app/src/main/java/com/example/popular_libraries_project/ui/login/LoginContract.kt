6d4e0652e3bf4a011f83fe962a4c768f7c2f3a38
Котик Дмитрий Степанович <DKotik@cbgr.ru>
2022-06-27 10:46:39 +0300

    lesson3 Добавил предварительный интерфейся для ViewModel

--- app/src/main/java/com/example/popular_libraries_project/ui/login/LoginContract.kt
+++ app/src/main/java/com/example/popular_libraries_project/ui/login/LoginContract.kt
@@ -1,6 +1,7 @@
 package com.example.popular_libraries_project.ui.login
 
 import androidx.annotation.MainThread
+import com.example.popular_libraries_project.utils.Publisher
 
 class LoginContract {
 
@@ -37,6 +38,27 @@ class LoginContract {
         // почти все методы начинаются на on...
         @MainThread
         fun onAttach(view: View)
+
+        @MainThread
+        fun onLogin(login: String, password: String)
+
+        @MainThread
+        fun onRegistration(login: String, password: String)
+
+        @MainThread
+        fun onForgotPassword()
+
+        @MainThread
+        fun onLogout()
+
+        @MainThread
+        fun onSendForgotPassword(email: String)
+    }
+
+    interface ViewModel {
+        val shouldShowProgress: Publisher<Boolean>
+        val isSuccess: Publisher<Boolean>
+        val errorText: Publisher<String?>
 
         @MainThread
         fun onLogin(login: String, password: String)
